c2f98a9051277f1e3f77bf535c98d783acd8585a
wordyallen <joe.narvaez@gmil.com>
2016-07-20 22:01:08 -0400

    resolved conflict

--- src/yelpButton.js
+++ src/yelpButton.js
@@ -2,13 +2,6 @@ import React from 'react'
 
 export default React.createClass({
   render (){
-<<<<<<< HEAD
-    return(
-      <a href="https://www.yelp.com/writeareview/biz/5hFFu5-9B5aXN2NahKt2qg?return_url=%2Fbiz%2F5hFFu5-9B5aXN2NahKt2qg" className='button is-danger is-large '>
-        <span className="icon"><i className="fa fa-yelp"></i></span>
-        <span>yelp</span>&nbsp;&nbsp;&nbsp;&nbsp;
-        </a>
-=======
     const url = 'https://www.yelp.com/writeareview/biz/5hFFu5-9B5aXN2NahKt2qg?return_url=%2Fbiz%2F5hFFu5-9B5aXN2NahKt2qg'
     return(
       <div>
@@ -17,7 +10,6 @@ export default React.createClass({
           <span>Review FVI on Yelp</span>&nbsp;&nbsp;&nbsp;&nbsp;
         </a>
       </div>
->>>>>>> 3c2e5a9534b11d590bfbc43e41cf7d09113a96b4
     )
   }
 })
