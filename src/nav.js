95423af8daf6f6e501c5563e21a4bd801ccb0a63
RandallSpence <randallspence@cyberservices.com>
2016-07-26 19:21:01 -0400

    nav bar

---
+++ src/nav.js
@@ -0,0 +1,23 @@
+import react from 'react'
+
+export default React.createClass({
+  render(){
+    return(
+      <nav class="nav has-shadow">
+  <div classNa="container">
+    <div classNam="nav-left">
+      <a className="nav-item is-tab is-active">Card</a>
+      <a className="nav-item is-tab">FaceBook</a>
+      <a className="nav-item is-tab">Yelp</a>
+      <a className="nav-item is-tab">tweeter</a>
+
+
+    </div>
+  </div>
+</nav>
+
+
+    )
+
+  }
+})
